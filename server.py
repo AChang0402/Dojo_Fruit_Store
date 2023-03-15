8b817acbeab8af1789d5a494e6d484eae2174f64
AChang0402 <alexchang.0402@gmail.com>
2023-03-14 20:32:27 -0700

    first commit

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)  
+
+@app.route('/')         
+def index():
+    return render_template("index.html")
+
+@app.route('/checkout', methods=['POST'])         
+def checkout():
+    print(request.form)
+    numstrawberries = int(request.form['strawberry'])
+    numraspberries = int(request.form['raspberry'])
+    numapples = int(request.form['apple'])
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    student_id = request.form['student_id']
+    count = numstrawberries + numraspberries + numapples
+    print(f"Charging {request.form['first_name']} for {count} fruits")
+    return render_template("checkout.html", numstrawberries = numstrawberries, numraspberries = numraspberries, numapples = numapples, first_name = first_name, last_name = last_name, student_id = student_id)
+
+@app.route('/fruits')         
+def fruits():
+    return render_template("fruits.html")
+
+if __name__=="__main__":   
+    app.run(debug=True)    
